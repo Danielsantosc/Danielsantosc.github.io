d63aa5fcc466f7a75fcf4ff690acf4b4f01feb07
Daniel Santos <dasaca98@gmail.com>
2017-07-14 20:13:58 -0300

    Estrutura alterada

---
+++ JS/main.js
@@ -0,0 +1,102 @@
+$(document).ready(function () {
+
+    aplicarBGAleatorio();
+
+    // Menu Hamburguer
+
+    $("#btAbrirGS").click(function () {
+        $("#menuHam").css({ "left": "0px", "opacity": "1" });
+    });
+
+    $(".closeMenuHam").click(function () {
+        $("#menuHam").css({ "left": "-100vw", "opacity": "0" });
+    });
+
+    // Menu Grid
+
+    $("#btAbrirGL").click(function () {
+        $("#menuGrid").css({ "right": "0px", "opacity": "1" });
+    });
+
+    $(".closeMenuGrid").click(function () {
+        $("#menuGrid").css({ "right": "-100vw", "opacity": "0" });
+    });
+
+    // Botão ver mais banner
+
+    $("#gtConteudo").click(function () {
+        var Banner = $("#banner").height();
+        $("html, body").animate({scrollTop : Banner - 75}, 850);
+    });
+
+});
+
+// função no scroll
+
+$(window).on("scroll", function () {
+
+    // Menu Fixo
+
+    var top = $(window).scrollTop();
+    var menuTopo = $("#menuTopo");
+    var menuTopoLink = $("#menuTopo #linksMenuTopo a");
+    var menuTopoImagensBranca = $("#menuTopo #socialMenuTopo a img.socialBranca");
+    var menuTopoImagensPreta = $("#menuTopo #socialMenuTopo a img.socialPreta");
+
+    if (top > 0) {
+        menuTopo.css({ "background-color" : "#ffffff", "box-shadow" : "0px 2px 2px rgba(0, 23, 31, 0.2), 0px 4px 4px rgba(0, 23, 31, 0.1)" });
+        menuTopoLink.css({ "color" : "#2E4147" });
+        menuTopoImagensBranca.css({ "display" : "none" });
+        menuTopoImagensPreta.css({ "display" : "block" });
+    } else {
+        menuTopo.css({ "background-color" : "transparent", "box-shadow" : "none" });
+        menuTopoLink.css({ "color" : "#ffffff" });
+        menuTopoImagensBranca.css({ "display" : "block" });
+        menuTopoImagensPreta.css({ "display" : "none" });
+    }
+
+    // parallax
+
+    if ($("#banner").length) {
+        $("#banner").each(function () {
+            var bgParallax = $(this);
+            var top = $(window).scrollTop();
+            var heightBanner = $("#banner").height();
+
+            $(window).scroll(function () {
+                if (top <= heightBanner) {
+                    var posBg = -(top / bgParallax.data("speed"));
+                    bgParallax.css({ "background-position": "50% " + posBg + "px" });
+                }
+            });
+        });
+    }
+});
+
+function aplicarBGAleatorio() {
+    var numeroBG = Math.floor(Math.random() * 5);
+
+    if ($("#banner").length) {
+        $("#banner").css({ "background-image": "url('Imagens/" + backgroundAleatorio(numeroBG) + "')" });
+    }
+
+    function backgroundAleatorio(x) {
+        switch (x) {
+            case 0:
+                return "background0.jpg";
+                break;
+            case 1:
+                return "background1.jpg";
+                break;
+            case 2:
+                return "background2.jpg";
+                break;
+            case 3:
+                return "background3.jpg";
+                break;
+            case 4:
+                return "background4.jpg";
+                break;
+        }
+    }
+}
